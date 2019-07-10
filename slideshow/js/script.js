acf69b2fca3b801d1f7c73818e9b8d0469522617
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2019-07-10 17:22:07 +0400

    Update script.js

--- slideshow/js/script.js
+++ slideshow/js/script.js
@@ -2,26 +2,13 @@ var i = 0; // dasawyisi
 	var images = []; // suratebis masibvi
 	var next = document.getElementsByClassName('next')
 	var back = document.getElementsByClassName('back')
-
 	// suratebis masivshi shetana
 	images[0] = '1.jpg';
 	images[1] = '2.jpg';
 	images[2] = '3.jpg';
 	images[3] = '4.jpg';
-
-	// slideshow 
-	function changeImg(){
-		document.slide.src = images[i];
-
-		if(i < images.length - 1){
-			i++;
-		} else {
-			i = 0;
-		}
-
-	}
-
-	window.onload = changeImg;
+	// next button js
+	window.onload = nextImg;
 		document.getElementById("myBtn").addEventListener("click", nextImg);
 		function nextImg(){
 			document.slide.src = images[i];
