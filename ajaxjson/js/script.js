edd1eb08c0e84bf99c2e374f02e2c5ce5a17b97e
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2019-07-14 18:39:12 +0400

    small

--- ajaxjson/js/script.js
+++ ajaxjson/js/script.js
@@ -1,23 +1,28 @@
-var dataContainer = document.getElementById("data-info");
+var dataContainer = document.getElementById("data-info"); // ვიღებთ ელემენტს აიდით და ვქმნით ცვლადს
 var btn = document.getElementById("btn");
+ // ვიღებთ ელემენტს აიდით და ვქმნით ცვლადს
 
-btn.addEventListener("click", function() {
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://jsonplaceholder.typicode.com/posts/');
-  ourRequest.onload = function() {
-      var ourData = JSON.parse(ourRequest.responseText);
-      renderHTML(ourData);
-      console.log(JSON.parse(ourRequest.responseText))
-  };
-  ourRequest.send();
-});
+function ajax() {
+    btn.addEventListener("click", function() { // ვამატებთ ღილაკის კლიკზე ფუნქციას
+        var ourRequest = new XMLHttpRequest(); // ვქმნით ცვლადს რათა წამოვიღოთ ინფორმაცია
+        ourRequest.open('GET', 'https://jsonplaceholder.typicode.com/posts/'); // GET-ით სერვერს ინფორმაციას ვართმევთ და მეორე პარამეტრით ვუთითებთ ლინკს საიდანააც ინფორმაცია უნდა წამოვიღოთ
+        ourRequest.onload = function() { // ინფორმაციის წამოღებისასვე შესრულდება ანონიმური ფუნქცია
+            var ourData = JSON.parse(ourRequest.responseText); // ვქმნიით ცვლადს სადაც ვიყენებთ ჩაშენებულ ფუნქციას, რომელიც ჩვენს წამოღებულ ინფორმაციას სტრინგიდან გადაიყვნას მასივში ან ობიექტში
+            createHTML(ourData); // ვიძახებთ ფუნქციას რომელსაც პარამეტრად გადეცემა ჩვენს მიერ შექმნილი ცვლაიდ
+            console.log(ourData) // ვამოწმებ ინფორმაციას
+        };
+        ourRequest.send(); // ვაგზავნით შეთავაზებას
+      });
+      
+}
 
-function renderHTML(data) {
-    var htmlString = "";
+window.onload = ajax();
 
-    for(i = 0; i < 3; i++) {
+function createHTML(data) { // ვქმნით ფუნქციას, რომელსაც პარამეტრად data-ს, რომელსაც გამოვიყენებთ ამავო ფუნქციაში 
+    var htmlString = ""; // ვქმნით ცვლადს რომელიც html-ში ჩაწერს ჩვენს წამოღებულ ინფორმაციას
+
+    for(i = 0; i < 3; i++) { // ვქმნით ციკლს სადაც ვატარებთ წამოღებული ინფორმაციის ნაწილს და ამ ნაწილებს ვუმატებთ სასურველ სტრინგს
         htmlString += "<p>" + data[i].title + "<span>" + " This was Title " + "</span>" + data[i].body + "<span>" + "This was body" + "</span> " + ".</p>"
     }
-
-    dataContainer.insertAdjacentHTML('beforeend', htmlString);
+    dataContainer.insertAdjacentHTML('beforeend', htmlString); // ამ მეთოდით ჩვენ ვირჩევთ ელემენტს html-ში და ამ ელემენტში ვსვავთ ზემოთ მოცემულ ციკლს
 }
