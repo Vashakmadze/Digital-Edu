cd287b2db6eafc523dde3da9e8321c1b9726492e
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2019-07-26 04:00:29 +0400

    angular

---
+++ angular/src/app/sectionnews/sectionnews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sectionnews',
+  templateUrl: './sectionnews.component.html',
+  styleUrls: ['./sectionnews.component.css']
+})
+export class SectionnewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
